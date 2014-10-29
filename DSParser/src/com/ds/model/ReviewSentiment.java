ca109b0a2f62634bb3041fe14ea80b40476d6f17
Biplap Sarkar <biplapsa@buffalo.edu>
2014-10-28 20:47:50 -0400

    Added class to represent review sentiment

---
+++ DSParser/src/com/ds/model/ReviewSentiment.java
@@ -0,0 +1,95 @@
+package com.ds.model;
+
+public class ReviewSentiment {
+	
+	public static final String BUSINESS_ID = "business_id";
+	public static final String USER_ID = "user_id";
+	public static final String TEXT = "text";
+	public static final String IS_POSITIVE_FOOD = "is_positive_food";
+	public static final String IS_POSITIVE_SERVICE = "is_positive_service";
+	public static final String IS_POSITIVE_AMBIENCE = "is_positive_ambience";
+	public static final String IS_POSITIVE_PRICE = "is_positive_price";
+	public static final String IS_NEGATIVE_FOOD = "is_negative_food";
+	public static final String IS_NEGATIVE_SERVICE = "is_negative_service";
+	public static final String IS_NEGATIVE_AMBIENCE = "is_negative_ambience";
+	public static final String IS_NEGATIVE_PRICE = "is_negative_price";
+	
+	private String businessId;
+	private String userId;
+	private String text;
+	private boolean isPositiveFood;
+	private boolean isPositiveService;
+	private boolean isPositiveAmbience;
+	private boolean isPositivePrice;
+	private boolean isNegativeFood;
+	private boolean isNegativeService;
+	private boolean isNegativeAmbience;
+	private boolean isNegativePrice;
+	public String getBusinessId() {
+		return businessId;
+	}
+	public void setBusinessId(String businessId) {
+		this.businessId = businessId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public boolean isPositiveFood() {
+		return isPositiveFood;
+	}
+	public void setPositiveFood(boolean isPositiveFood) {
+		this.isPositiveFood = isPositiveFood;
+	}
+	public boolean isPositiveService() {
+		return isPositiveService;
+	}
+	public void setPositiveService(boolean isPositiveService) {
+		this.isPositiveService = isPositiveService;
+	}
+	public boolean isPositiveAmbience() {
+		return isPositiveAmbience;
+	}
+	public void setPositiveAmbience(boolean isPositiveAmbience) {
+		this.isPositiveAmbience = isPositiveAmbience;
+	}
+	public boolean isPositivePrice() {
+		return isPositivePrice;
+	}
+	public void setPositivePrice(boolean isPositivePrice) {
+		this.isPositivePrice = isPositivePrice;
+	}
+	public boolean isNegativeFood() {
+		return isNegativeFood;
+	}
+	public void setNegativeFood(boolean isNegativeFood) {
+		this.isNegativeFood = isNegativeFood;
+	}
+	public boolean isNegativeService() {
+		return isNegativeService;
+	}
+	public void setNegativeService(boolean isNegativeService) {
+		this.isNegativeService = isNegativeService;
+	}
+	public boolean isNegativeAmbience() {
+		return isNegativeAmbience;
+	}
+	public void setNegativeAmbience(boolean isNegativeAmbience) {
+		this.isNegativeAmbience = isNegativeAmbience;
+	}
+	public boolean isNegativePrice() {
+		return isNegativePrice;
+	}
+	public void setNegativePrice(boolean isNegativePrice) {
+		this.isNegativePrice = isNegativePrice;
+	}
+	
+}
